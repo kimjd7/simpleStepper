f30a49e1edf23a3d6d9ed0fec465e5d69720537d
kimjd7 <kimjd@hallym.ac.kr>
2022-05-09 18:28:03 +0900

    2차 수정

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,49 @@
-self.addEventListener('fetch', function(e) {
-  // 여기에 코드를 넣지 않아도 설치는 가능하다.
-})
+var CACHE_NAME = 'pwa-example-basic';
+var filesToCache = [
+  '/',
+  '/css/app.css',
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+      .catch(function(error) {
+        return console.log(error);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetch');
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+      .catch(function(error) {
+        return console.log(error);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var newCacheList = ['pwa-example-basic'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheList) {
+      return Promise.all(
+        cacheList.map(function(cacheName) {
+          if (newCacheList.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    }).catch(function(error) {
+      return console.log(error);
+    })
+  );
+});
